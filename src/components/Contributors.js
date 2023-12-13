c77da621b828073f05e9d41fa839b7e3616696ed
JitHoon <imessi0336@gmail.com>
2023-12-13 22:16:16 +0900

    feat: 기여자 추가

--- src/components/Contributors.js
+++ src/components/Contributors.js
@@ -218,6 +218,46 @@ export default function Contributors() {
           </div>
         </div>
       </div>
+      <hr width="90%" color="gray"></hr>
+      <div className={styles.contributor}>
+        <div className={styles.contributor_imgbox}>
+          <img
+            id={styles.contributor_img}
+            src="https://avatars.githubusercontent.com/u/57122180?v=4"
+            alt="김민수"
+            width="250px"
+            height="300px"
+            loading="lazy"
+            decoding="async"
+          />
+        </div>
+        <div className={styles.contributor_contents}>
+          <h3 className={styles.contributor_name}>김민수</h3>
+          <div className={styles.contributor_fe}>프론트엔드 개발자</div>
+          <p className={styles.contributor_desc}>
+            공유의 가치를 믿는 개발자, 김민수입니다.
+          </p>
+          <div className={styles.contributor_github}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="1.2em"
+              height="1.2em"
+              viewBox="0 -2 24 24"
+              fill="currentColor"
+            >
+              <path d="M10 0a10 10 0 0 0-3.16 19.49c.5.1.68-.22.68-.48l-.01-1.7c-2.78.6-3.37-1.34-3.37-1.34-.46-1.16-1.11-1.47-1.11-1.47-.9-.62.07-.6.07-.6 1 .07 1.53 1.03 1.53 1.03.9 1.52 2.34 1.08 2.91.83.1-.65.35-1.09.63-1.34-2.22-.25-4.55-1.11-4.55-4.94 0-1.1.39-1.99 1.03-2.69a3.6 3.6 0 0 1 .1-2.64s.84-.27 2.75 1.02a9.58 9.58 0 0 1 5 0c1.91-1.3 2.75-1.02 2.75-1.02.55 1.37.2 2.4.1 2.64.64.7 1.03 1.6 1.03 2.69 0 3.84-2.34 4.68-4.57 4.93.36.31.68.92.68 1.85l-.01 2.75c0 .26.18.58.69.48A10 10 0 0 0 10 0"></path>
+            </svg>
+            <a
+              href="https://github.com/minsoo-web"
+              target="_blank"
+              rel="noopener"
+              aria-label="GitHub Profile"
+            >
+              minsoo-web
+            </a>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
